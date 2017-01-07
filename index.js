daebe963840e3563705ca0ecc0e67116e32495f0
Derek Vincent <derek.vincent@kemikal.net>
2017-01-07 13:33:58 -0500

    Commit Changes to Repo before package rename.

---
+++ index.js
@@ -0,0 +1,116 @@
+/**
+ * Created by derekvincent on 2017-01-03.
+ */
+'use strict';
+
+
+
+module.exports = EventTime;
+
+/**
+ * Constructor for the duration time via a formatted string HH:MM:ss.ss
+ * @param {string} time
+ */
+function EventTime(time) {
+
+    // need to check if set
+    // if not then default to 00:00:00.0
+    this.setTime(time);
+}
+
+/**
+ * Set the duration time via a formatted string HH:MM:ss.s
+ * @method setTime
+ * @param {string} time
+ */
+EventTime.prototype.setTime = function(time) {
+
+    var nInternalTime = convertToRaw(time);
+    this.rawTime =  nInternalTime;
+
+};
+/**
+ * Gets the time duration in the string formatted value
+ * @return {string} formmated value
+ */
+EventTime.prototype.getTime = function() {
+
+    return convertToFormat(this.rawTime);
+};
+
+/**
+ * Adds the passed in value (formatted duration string) and returns the raw duration. It does not affect the internal
+ * storage of the time duration.
+ * @param {string} time
+ * @return {number} sum
+ */
+EventTime.prototype.addTime = function(time) {
+
+    // Not sure if we want to just calculate the add or to store it
+    var inTime = convertToRaw(time);
+    var totalTime = this.rawTime + inTime;
+    return totalTime;
+};
+
+/**
+ *
+ * @param time
+ * @return {number}
+ */
+EventTime.prototype.diffTimeRaw = function(time) {
+    var inTime = convertToRaw(time);
+    var totalDiffernce = this.rawTime - inTime;
+    return totalDiffernce;
+}
+
+// This Function might not stay or maybe usefull in the sorter
+EventTime.prototype.getRawtime = function() {
+    return this.rawTime;
+};
+
+var convertToRaw = function(time) {
+
+    // should be checking for valid format before starting
+    // regx would be fine
+    var aTimeBreakdown = time.split(":");
+    var sHour,
+        sMinute,
+        sSecond,
+        nInternalTime;
+    if (aTimeBreakdown.length  > 2) {
+        sHour = aTimeBreakdown[0];
+        sMinute = aTimeBreakdown[1];
+        sSecond = aTimeBreakdown[2];
+    } else if (aTimeBreakdown.length = 2 ) {
+        sHour = '00';
+        sMinute = aTimeBreakdown[1];
+        sSecond = aTimeBreakdown[2];
+
+    }
+    nInternalTime = (Number(sHour) * 3600) + (Number(sMinute) * 60) + Number(sSecond);
+    return nInternalTime;
+};
+
+var convertToFormat = function(rawTime) {
+
+    var sHours,
+        sMinutes,
+        sSeconds = "00";
+
+    sHours = Math.floor(rawTime /  3600);
+    rawTime %= 3600;
+    sMinutes = Math.floor(rawTime  / 60);
+    sSeconds = rawTime % 60;
+
+    console.log("Raw Time: " + rawTime + " Converted Time: " + padTimeSegment(sHours) + ":" + padTimeSegment(sMinutes) + ":" + padTimeSegment(sSeconds));
+    return padTimeSegment(sHours) + ":" + padTimeSegment(sMinutes) + ":" + padTimeSegment(sSeconds);
+};
+
+var padTimeSegment = function(timeSegment) {
+
+    // This is very specific to the time segment if the field is less
+    // then 1 character long it will add a 0 to left side.
+    var wholeSegment = timeSegment.toString().split('.')[0];
+    return wholeSegment.length === 1 ? "0" + timeSegment : timeSegment;
+
+};
---
+++ test/test.js
@@ -0,0 +1,154 @@
+'use strict';
+
+var expect = require('chai').expect;
+var eventTime = require('../index');
+
+describe('EventTime', function(){
+    describe('Simple format 00:00:00 return Internal Format', function() {
+
+        it('should take one second', function() {
+            var result = new eventTime("00:00:01");
+            expect(result.getRawtime()).to.equal(1);
+        });
+
+        it('should take ten seconds', function() {
+            var result = new eventTime("00:00:10");
+            expect(result.getRawtime()).to.equal(10);
+        });
+
+        it('should take ten seconds and 1 tenths of a second', function() {
+            var result = new eventTime("00:00:10.1");
+            expect(result.getRawtime()).to.equal(10.1);
+        });
+
+        it('should take one minute', function() {
+            var result = new eventTime("00:01:00");
+            expect(result.getRawtime()).to.equal(60);
+        });
+
+        it('should take ten minutes', function() {
+            var result = new eventTime("00:10:00");
+            expect(result.getRawtime()).to.equal(600);
+        });
+
+        it('should take one minute and 10 second', function() {
+            var result = new eventTime("00:01:10");
+            expect(result.getRawtime()).to.equal(70);
+        });
+
+        it('should take ten minutes and 30 seconds', function() {
+            var result = new eventTime("00:10:30");
+            expect(result.getRawtime()).to.equal(630);
+        });
+
+    });
+
+    describe('Simple format 00:00:00 return Proper Format', function() {
+
+        it('should take one tenth of a second', function() {
+            var time = "00:00:00.1";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one second', function() {
+            var time = "00:00:01";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take ten seconds', function() {
+            var time = "00:00:10";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one seconds and 1 tenths of a second', function() {
+            var time = "00:00:01.1";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one seconds and one hunderth of a second', function() {
+            var time = "00:00:01.01";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take ten seconds and 10 tenths of a second', function() {
+            var time = "00:00:10.1";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one minute', function() {
+            var time = "00:01:00";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take ten minutes', function() {
+            var time = "00:10:00";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one minute and 10 second', function() {
+            var time = "00:01:10";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take ten minutes and 30 seconds', function() {
+            var time = "00:10:30";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one hour ', function() {
+            var time = "01:00:00";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one hour and one minute', function() {
+            var time = "01:01:00";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one hour, one minutes and one second', function() {
+            var time = "01:01:01";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one hour and ten minutes', function() {
+            var time = "01:10:00";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+        it('should take one hour, ten minutes and one second', function() {
+            var time = "01:10:01";
+            var result = new eventTime(time);
+            expect(result.getTime()).to.equal(time);
+        });
+
+    });
+    describe('Adding Time', function() {
+        it('should take ten minutes and add 30 minutes Return raw', function() {
+            var eventTimeToken = new eventTime("00:10:00");
+            var result =  eventTimeToken.addTime(("0:30:00"));
+            expect(result).to.equal(2400);
+            expect(eventTimeToken.getRawtime()).to.equal(2400);
+        });
+
+        it('should take ten minutes and add 30 minutes Return formatted', function() {
+            var eventTimeToken = new eventTime("00:10:00");
+            var result =  eventTimeToken.addTime(("0:30:00"));
+            expect(result).to.equal(2400);
+            expect(eventTimeToken.getRawtime()).to.equal(2400);
+        });
+    })
+});
