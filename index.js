c1443e63a3fc581ae356cd8da09a2d03a1c32a17
Derek Vincent <derek.vincent@kemikal.net>
2017-03-19 11:10:18 -0400

    Commit after package name change and package/code adjustments

--- index.js
+++ index.js
@@ -5,25 +5,42 @@
 
 
 
-module.exports = TimePeriod;
+module.exports = Duration;
+
+// Set the Millisecond precision of decimal points. Default to 2.
+var nPrecision = 1;
 
 /**
  * Constructor for the duration time via a formatted string HH:MM:ss.ss
  * @param {string} time
  */
-function TimePeriod(time) {
+function Duration(time, endTime) {
 
-    // need to check if set
-    // if not then default to 00:00:00.0
-    this.setTime(time);
+    if (time === undefined) {
+        // If time is not defined then assume an empty object and the time 00:00:00
+        this.setTime("00:00:00");
+    }
+     else if (endTime === undefined) {
+        // If only time is set the assume we are looking a duration and set it
+        this.setTime(time);
+    } else if (time !== undefined && endTime !== undefined) {
+        // If both time and endTime are entered the assume we need to calutlate the duration a difference of the two
+        this.setDurationFromStartEndTimes(time, endTime);
+    } else {
+        //Throw and error of some sort!
+    }
+
+
 }
+
+
 
 /**
  * Set the duration time via a formatted string HH:MM:ss.s
  * @method setTime
  * @param {string} time
  */
-TimePeriod.prototype.setTime = function(time) {
+Duration.prototype.setTime = function(time) {
 
     var nInternalTime = convertToRaw(time);
     this.rawTime =  nInternalTime;
@@ -33,7 +50,7 @@ TimePeriod.prototype.setTime = function(time) {
  * Gets the time duration in the string formatted value
  * @return {string} formmated value
  */
-TimePeriod.prototype.getTime = function() {
+Duration.prototype.getTime = function() {
 
     return convertToFormat(this.rawTime);
 };
@@ -44,7 +61,7 @@ TimePeriod.prototype.getTime = function() {
  * @param {string} time
  * @return {number} sum
  */
-TimePeriod.prototype.addTime = function(time) {
+Duration.prototype.addTime = function(time) {
 
     // Not sure if we want to just calculate the add or to store it
     var inTime = convertToRaw(time);
@@ -57,21 +74,52 @@ TimePeriod.prototype.addTime = function(time) {
  * @param time
  * @return {number}
  */
-TimePeriod.prototype.diffTimeRaw = function(time) {
+Duration.prototype.diffTimeRaw = function(time) {
     var inTime = convertToRaw(time);
     var totalDifference = this.rawTime - inTime;
     return totalDifference;
 };
 
 // This Function might not stay or maybe usefull in the sorter
-TimePeriod.prototype.getRawtime = function() {
+Duration.prototype.getRawTime = function() {
     return this.rawTime;
 };
 
+/**
+ * Takes the start and end time (in HH:MM:ss.mmm format) and stores the duration between them.
+ * If the end time is less then the start time then 12 hours is added to take of the AM/PM issue on the
+ * same day. It does not handle roll over on days.
+ * @param {string} startTime
+ * @param {string} endTime
+ *
+ */
+Duration.prototype.setDurationFromStartEndTimes = function(startTime, endTime) {
+    var nStartTimeRaw = convertToRaw(startTime),
+        nEndTimeRaw = convertToRaw(endTime),
+        nDuration = 0;
+
+    if ( nStartTimeRaw > nEndTimeRaw ) {
+        nEndTimeRaw += 43200000;
+    }
+    this.rawTime = nEndTimeRaw - nStartTimeRaw;
+    console.log("Start Time: " + startTime + " Raw: " + nStartTimeRaw + " End Time: " + endTime + " Raw: " + nEndTimeRaw
+        + " Duration: " + this.getTime() + " Raw Duration: " + this.getRawTime());
+
+};
+
+Duration.prototype.setPercision = function(nNumber) {
+    if (nNumber < 4) {
+        nPrecision = nNumber;
+    }
+};
+
+Duration.prototype.getPercision = function(){
+    return nPrecision;
+}
+
 var convertToRaw = function(time) {
 
-    // should be checking for valid format before starting
-    // regx would be fine
+    //TODO: should be checking for valid format before starting - regx would be fine
     var aTimeBreakdown = time.split(":");
     var sHour,
         sMinute,
@@ -87,30 +135,34 @@ var convertToRaw = function(time) {
         sSecond = aTimeBreakdown[2];
 
     }
-    nInternalTime = (Number(sHour) * 3600) + (Number(sMinute) * 60) + Number(sSecond);
-    return nInternalTime;
+    var nHours = Number(sHour) * 3600 * 1000;
+    var nMinutes = Number(sMinute) * 60 * 1000;
+    var nSeconds = Number(sSecond) * 1000;
+    nInternalTime = nHours + nMinutes + nSeconds;
+
+    return parseFloat(nInternalTime);
 };
 
 var convertToFormat = function(rawTime) {
 
-    var sHours,
-        sMinutes,
-        sSeconds;
-
-    sHours = Math.floor(rawTime /  3600);
-    rawTime %= 3600;
-    sMinutes = Math.floor(rawTime  / 60);
-    sSeconds = rawTime % 60;
-
-    console.log("Raw Time: " + rawTime + " Converted Time: " + padTimeSegment(sHours) + ":" + padTimeSegment(sMinutes) + ":" + padTimeSegment(sSeconds));
-    return padTimeSegment(sHours) + ":" + padTimeSegment(sMinutes) + ":" + padTimeSegment(sSeconds);
+    var time = new Date(rawTime);
+    var timeString =  padTime(time.getUTCHours()) + ":"
+                    + padTime(time.getUTCMinutes()) + ":"
+                    + padTime(time.getUTCSeconds())
+                    + formatMilli(time.getUTCMilliseconds());
+    return timeString;
 };
 
-var padTimeSegment = function(timeSegment) {
+var padTime = function(timeSegment) {
 
-    // This is very specific to the time segment if the field is less
-    // then 1 character long it will add a 0 to left side.
-    var wholeSegment = timeSegment.toString().split('.')[0];
-    return wholeSegment.length === 1 ? "0" + timeSegment : timeSegment;
+    if (timeSegment < 10) {
+        return "0" + timeSegment;
+    }
+    return timeSegment
 
 };
+
+var formatMilli = function(milliseconds) {
+
+    return parseFloat((milliseconds/1000).toFixed(nPrecision)).toString().slice(1, (nPrecision +2));
+}
--- test/test.js
+++ test/test.js
@@ -3,44 +3,63 @@
 var expect = require('chai').expect;
 var TimePeriod = require('../index');
 
-describe('EventTime', function(){
+describe('Duration', function(){
     describe('Simple format 00:00:00 return Internal Format', function() {
+
+        it ('should take an empty constructor', function() {
+            var result = new TimePeriod();
+            expect(result.getRawTime()).to.equal(0);
+        });
 
         it('should take one second', function() {
             var result = new TimePeriod("00:00:01");
-            expect(result.getRawtime()).to.equal(1);
+            expect(result.getRawTime()).to.equal(1 * 1000);
         });
 
         it('should take ten seconds', function() {
             var result = new TimePeriod("00:00:10");
-            expect(result.getRawtime()).to.equal(10);
+            expect(result.getRawTime()).to.equal(10 * 1000);
         });
 
         it('should take ten seconds and 1 tenths of a second', function() {
             var result = new TimePeriod("00:00:10.1");
-            expect(result.getRawtime()).to.equal(10.1);
+            expect(result.getRawTime()).to.equal(10.1 * 1000);
         });
 
         it('should take one minute', function() {
             var result = new TimePeriod("00:01:00");
-            expect(result.getRawtime()).to.equal(60);
+            expect(result.getRawTime()).to.equal(60 * 1000);
         });
 
         it('should take ten minutes', function() {
             var result = new TimePeriod("00:10:00");
-            expect(result.getRawtime()).to.equal(600);
+            expect(result.getRawTime()).to.equal(600 * 1000);
         });
 
         it('should take one minute and 10 second', function() {
             var result = new TimePeriod("00:01:10");
-            expect(result.getRawtime()).to.equal(70);
+            expect(result.getRawTime()).to.equal(70 * 1000);
         });
 
         it('should take ten minutes and 30 seconds', function() {
             var result = new TimePeriod("00:10:30");
-            expect(result.getRawtime()).to.equal(630);
+            expect(result.getRawTime()).to.equal(630 * 1000);
         });
 
+        it('should take one hour', function() {
+            var result = new TimePeriod("01:00:00");
+            expect(result.getRawTime()).to.equal(3600 * 1000);
+        });
+
+        it('should take one hour and ten minutes', function() {
+            var result = new TimePeriod("01:10:00");
+            expect(result.getRawTime()).to.equal(4200 * 1000);
+        });
+
+        it('should take one hour, ten minutes and ten seconds', function() {
+            var result = new TimePeriod("01:10:10");
+            expect(result.getRawTime()).to.equal(4210 * 1000);
+        });
     });
 
     describe('Simple format 00:00:00 return Proper Format', function() {
@@ -54,6 +73,7 @@ describe('EventTime', function(){
         it('should take one second', function() {
             var time = "00:00:01";
             var result = new TimePeriod(time);
+            result.setPercision(2);
             expect(result.getTime()).to.equal(time);
         });
 
@@ -69,7 +89,7 @@ describe('EventTime', function(){
             expect(result.getTime()).to.equal(time);
         });
 
-        it('should take one seconds and one hunderth of a second', function() {
+        it('should take one seconds and one hundreth of a second', function() {
             var time = "00:00:01.01";
             var result = new TimePeriod(time);
             expect(result.getTime()).to.equal(time);
@@ -136,17 +156,107 @@ describe('EventTime', function(){
         });
 
     });
+
+    describe('Creation of duration using start and end times', function(){
+        it('should start at 12:00:00 and finish at 12:00:01', function() {
+           var result = new TimePeriod("12:00:00", "12:00:01");
+           expect(result.getTime()).to.equal("00:00:01");
+        });
+
+        it('should start at 12:00:00 and finish at 12:01:01', function() {
+            var result = new TimePeriod("12:00:00", "12:01:01");
+            expect(result.getTime()).to.equal("00:01:01");
+        });
+
+        it('should start at 12:00:00 and finish at 12:10:10', function() {
+            var result = new TimePeriod("12:00:00", "12:10:10");
+            expect(result.getTime()).to.equal("00:10:10");
+        });
+
+        it('should start at 12:00:00 and finish at 12:10:10.5', function() {
+            var result = new TimePeriod("12:00:00", "12:10:10.5");
+            expect(result.getTime()).to.equal("00:10:10.5");
+        });
+
+        it('should start at 12:55:00 and finish at 1:10:10.5', function() {
+            var result = new TimePeriod("12:55:00", "01:10:10.5");
+            expect(result.getTime()).to.equal("00:15:10.5");
+        });
+
+        it('create an empty object and set the start at 11:55:00 and finish at 12:10:10.5', function() {
+            var result = new TimePeriod();
+            result.setDurationFromStartEndTimes("11:55:00", "12:10:10.5");
+            expect(result.getTime()).to.equal("00:15:10.5");
+        });
+    });
+
     describe('Adding Time', function() {
+
+        it('should take ten second and add two seconds', function() {
+            var timePeriodToken = new TimePeriod("00:00:10");
+            var result =  timePeriodToken.addTime(("0:00:02"));
+            expect(result).to.equal(12 * 1000);
+        });
+
+        it('should take fifty seconds and add twelve seconds', function() {
+            var timePeriodToken = new TimePeriod("00:00:50");
+            var result =  timePeriodToken.addTime(("0:00:12"));
+            expect(result).to.equal(62 * 1000);
+        });
+
         it('should take ten minutes and add 30 minutes Return raw', function() {
             var timePeriodToken = new TimePeriod("00:10:00");
             var result =  timePeriodToken.addTime(("0:30:00"));
-            expect(result).to.equal(2400);
+            expect(result).to.equal(2400 * 1000);
         });
 
-        it('should take ten minutes and add 30 minutes Return formatted', function() {
-            var timePeriodToken = new TimePeriod("00:10:00");
-            var result =  timePeriodToken.addTime(("0:30:00"));
-            expect(result).to.equal(2400);
+        it('should take fifty minutes and add twleve minutes', function() {
+            var timePeriodToken = new TimePeriod("00:50:00");
+            var result =  timePeriodToken.addTime(("0:12:00"));
+            expect(result).to.equal(3720 * 1000);
         });
-    })
+
+        it('should take fifty minutes thirty seconds and add ten minutes', function() {
+            var timePeriodToken = new TimePeriod("00:50:30");
+            var result =  timePeriodToken.addTime(("0:10:00"));
+            expect(result).to.equal(3630 * 1000);
+        });
+
+        it('should take one hour and add one second', function() {
+            var timePeriodToken = new TimePeriod("01:00:00");
+            var result =  timePeriodToken.addTime(("0:00:01"));
+            expect(result).to.equal(3601 * 1000);
+        });
+
+        it('should take one hour and add one minute', function() {
+            var timePeriodToken = new TimePeriod("01:00:00");
+            var result =  timePeriodToken.addTime(("0:01:00"));
+            expect(result).to.equal(3660 * 1000);
+        });
+
+        it('should take one hour and add 10 hours', function() {
+            var timePeriodToken = new TimePeriod("01:00:00");
+            var result =  timePeriodToken.addTime(("10:00:00"));
+            expect(result).to.equal(39600 * 1000);
+        });
+    });
+    describe('Difference Time', function() {
+        it('should calculate the difference between 5 second and 4 seconds', function() {
+            var timePeriodToken = new TimePeriod("00:00:05");
+            var result = timePeriodToken.diffTimeRaw("00:00:05");
+            expect(result).to.equal(0 * 1000);
+        });
+
+        it('should calculate the difference between 5 second and 4 seconds', function() {
+            var timePeriodToken = new TimePeriod("00:00:05");
+            var result = timePeriodToken.diffTimeRaw("00:00:04");
+            expect(result).to.equal(1 * 1000);
+        });
+
+        it('should calculate the difference between 5 second and 4 seconds', function() {
+            var timePeriodToken = new TimePeriod("00:00:05");
+            var result = timePeriodToken.diffTimeRaw("00:00:04.1");
+            expect(result).to.equal(0.9 * 1000);
+        });
+    });
 });
